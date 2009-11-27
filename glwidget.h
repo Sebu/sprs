2c8f6545f21efba61c551e0829a631329eaa7b48
Sebastian Szczepanski <millennium-x@gmx.net>
2009-11-27 02:43:07 +0100

    opencv goodFeatures detect

--- glwidget.h
+++ glwidget.h
@@ -18,16 +18,34 @@ public:
     GLWidget(QWidget *parent)
             : QGLWidget(parent) {
         glGenTextures(1, &texImage);
-        setMinimumSize(300,300);
+        setMinimumSize(400,400);
     }
 
     int fromIpl(IplImage *image) {
         if (image==NULL) return -1;
 
         glBindTexture( GL_TEXTURE_2D, texImage );
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image->width, image->height, 0, GL_BGR, GL_UNSIGNED_BYTE, image->imageData);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+
+        GLenum input_format;
+
+        switch(image->nChannels) {
+            case 1: input_format = GL_LUMINANCE; break;
+            case 3: input_format = GL_BGR; break;
+            case 4: input_format = GL_BGRA; break;
+            default: input_format = GL_BGR; break;
+        }
+
+        GLenum channel_depth;
+
+        switch(image->depth) {
+            case IPL_DEPTH_8U: channel_depth  = GL_UNSIGNED_BYTE; break;
+            case IPL_DEPTH_32F: channel_depth = GL_FLOAT; break;
+            default: channel_depth = GL_UNSIGNED_BYTE; break;
+        }
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image->width, image->height, 0, input_format, channel_depth, image->imageData);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
 
         return 0;
 
@@ -39,25 +57,20 @@ public slots:
 protected:
     void initializeGL()
     {
-        // Set up the rendering context, define display lists etc.:
         glEnable(GL_TEXTURE_2D);
         glClearColor( 0.5f, 0.5f, 0.5f, 0.0f );
-
     }
 
     void resizeGL(int w, int h)
     {
-        // setup viewport, projection etc.:
         glViewport(0, 0, (GLint)w, (GLint)h);
     }
 
     void paintGL()
     {
-        // draw the scene:
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// Clear The Screen And The Depth Buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glLoadIdentity();
 
-        //Attempts to draw texture to the opened window.
         glBegin(GL_QUADS);
         glTexCoord2f(0.0f, 0.0f); glVertex3f(-1.0f, 1.0f, 0.0f);
         glTexCoord2f(1.0f, 0.0f); glVertex3f( 1.0f, 1.0f, 0.0f);
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -19,11 +19,36 @@ MainWindow::MainWindow(QWidget *parent)
 }
 void MainWindow::changeImage()
 {
+    IplImage *frame1 = 0;
+    IplImage* eig_image = 0;
+    IplImage* temp_image = 0;
+
     if (image!=NULL) cvReleaseImage(&image);
     QString fileName = QFileDialog::getOpenFileName(this,tr("Open Image"), "/home/seb", tr("Image Files (*.png *.jpg *.bmp)"));
     image =  cvLoadImage(fileName.toAscii());
 
-    this->imageWidget->fromIpl(image);
+    int w = image->width;
+    int h = image->height;
+
+    frame1 = cvCreateImage( cvSize(w, h), IPL_DEPTH_8U, 1);
+    eig_image = cvCreateImage( cvSize(w, h), IPL_DEPTH_32F, 1);
+    temp_image = cvCreateImage( cvSize(w, h), IPL_DEPTH_32F, 1);
+
+
+    cvConvertImage( image, frame1 );
+
+    int N = 1000;
+    CvPoint2D32f frame1_features[N];
+
+    cvGoodFeaturesToTrack(frame1, eig_image, temp_image, frame1_features, &N, .01, .01, NULL);
+
+//    for( int i = 0; i < N; i++) {
+//        int radius = w/25.0f;
+//        cvCircle(image, cvPoint((int)(frame1_features[i].x + 0.5f),(int)(frame1_features[i].y + 0.5f)), radius,	cvScalar(0,255,0));
+//    }
+
+    cvScale(eig_image, eig_image, 100, 0.00);
+    this->imageWidget->fromIpl(eig_image);
 
 }
 
