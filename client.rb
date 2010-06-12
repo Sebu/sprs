9d5374cced2dcfa37dc7ecf5785db943eb99eac2
Sebastian Szczepanski <seb@c18.math.tu-berlin.de>
2010-06-12 19:55:36 +0200

    client.rb

--- client.rb
+++ client.rb
@@ -4,5 +4,5 @@ file_name = ARGV[0]
 max_error = ARGV[1] || 0.5
 other_args = ARGV[2..-1]
 puts file_name
-exec "LD_LIBRARY_PATH=~/git/epitome/epicore:~/playground/lib:/usr/local/lib; ~/git/epitome/epicli/epicli -i #{file_name} -e #{max_error} #{other_args}"
+exec "export LD_LIBRARY_PATH=~/git/epitome/epicore:~/playground/lib:/usr/local/lib; ~/git/epitome/epicli/epicli -i #{file_name} -e #{max_error} #{other_args}"
 
