7f0fbc3b92e43d6aa6cc1b77f78aa9d5f4608daa
Sebastian Szczepanski <millennium-x@gmx.net>
2010-06-23 23:32:41 +0200

    stuffing

--- epicore/searchcriteria.cpp
+++ epicore/searchcriteria.cpp
@@ -2,7 +2,7 @@
 
 SearchCriteria::SearchCriteria() :
         alpha_(1.2f), maxError_(0.16f), maxColor_(1.5f), maxOrient_(.5f),
-        gfNumFeatures_(10), gfQualityLvl_(0.02), gfMinDist_(1.0),
+        gfNumFeatures_(10), gfQualityLvl_(0.01), gfMinDist_(1.0),
         kltWinSize_(6), kltMaxLvls_(1)
 {
 }
