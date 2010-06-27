2aa1dd2a538b9f0e3e3618b20c692d277d0a5817
Sebastian Szczepanski <millennium-x@gmx.net>
2010-06-28 00:22:27 +0200

    wurst

--- epicore/searchcriteria.cpp
+++ epicore/searchcriteria.cpp
@@ -1,7 +1,7 @@
 #include "searchcriteria.h"
 
 SearchCriteria::SearchCriteria() :
-        alpha_(1.4f), maxError_(0.16f), maxColor_(1.5f), maxOrient_(.6f),
+        alpha_(1.0f), maxError_(0.16f), maxColor_(1.5f), maxOrient_(.6f),
         gfNumFeatures_(10), gfQualityLvl_(0.01), gfMinDist_(1.0),
         kltWinSize_(5), kltMaxLvls_(1)
 {
