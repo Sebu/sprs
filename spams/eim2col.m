e0bf38065c819470dc02d37b3fd8c000878d0d62
Sebastian Szczepanski <seb@c63.math.tu-berlin.de>
2010-07-02 16:03:15 +0200

    multi channel (e)im2col

--- spams/eim2col.m
+++ spams/eim2col.m
@@ -1,17 +1,36 @@
-function M = eim2col(I, m, n)
+function X = eim2col(O, D, step)
 
-[rows, cols] = size(I);
-mm = rows / m;
-nn = cols / n;
-M = zeros(m*n,mm*nn);
 
-k = 1;
-for y=0:mm-1
-    for x=0:nn-1
-        indexy = y*m;
-        indexx = x*n;
-        C = I(indexy+1:indexy+m,indexx+1:indexx+n);
-        M(:,k) = C(:);
-        k=k+1;
+
+[height, width, channels] = size(O);
+m = D(1);
+n = D(2);
+mm = ceil(height / step);
+nn = ceil(width / step);
+I = zeros([mm*step nn*step channels]);
+I(1:height, 1:width,:) = O;
+
+X = [];
+for c=1:channels
+    T =  I(:,:,c);
+    maxm = mm-(m/step);
+    maxn = nn-(n/step);
+    M = ones([m*n, maxm*maxn]);
+    k = 1;
+    for y=0:maxm
+        for x=0:maxn
+            indexy = y*step;
+            indexx = x*step;
+            C = T(indexy+1:indexy+m,indexx+1:indexx+n);
+            M(:,k) = C(:);
+            k=k+1;
+        end;
     end;
-end;
+    if isempty(X)
+        X = M;
+    else
+        X = cat(1,X,M);
+    end
+end
+
+
--- spams/epi.m
+++ spams/epi.m
@@ -1,72 +1,41 @@
 clear all;
 
-s_ = 16;
+s_ = 12;
 
-I1=double(imread('~/Bilder/factorimage_data/fig20c_orig.png')) /255;
-[height, width, channels] = size(I1);
-X = [];
-for c=1:channels
-    T =  I1(:,:,c);
-    T2 = im2col(T, [s_  s_], 'distinct');
-    if isempty(X)
-        X = T2;
-    else
-        X = cat(1,X,T2);
-    end
-end
-I2=double(imread('~/Bilder/factorimage_data/fig20c_epitome.png'))/255;
-[height, width, channels] = size(I2);
-X2 = [];
-for c=1:channels
-    T =  I2(:,:,c);
-    T2 = im2col(T, [s_  s_], 'sliding');
-    if isempty(X2)
-        X2 = T2;
-    else
-        X2 = cat(1,X2,T2);
-    end
-end
+I1=double(imread('~/Bilder/factorimage_data/bild5.jpg')) /255;
+X1 = eim2col(I1, [s_ s_], s_);
+
+I2=double(imread('~/Bilder/factorimage_data/epitomes/bild5.jpg.epi.png'))/255;
+X2= eim2col(I2, [s_ s_], s_/4);
 X2=X2./repmat(sqrt(sum(X2.^2)),[size(X2,1) 1]);
 
 
-param.K=500;  % learns a dictionary with 100 elements
+param.K=100;  % learns a dictionary with 100 elements
 param.lambda=0.15;
-% param.numThreads=4; % number of threads
-%param.batchsize=1000;
+% param.batchsize=1000;
 
-param.iter=100;  % let us see what happens after 100 iterations.
+param.iter=500;  % let us see what happens after 100 iterations.
 
-
-%%%%%%%%%% FIRST EXPERIMENT %%%%%%%%%%%
 tic
-D = mexTrainDL(X2,param);
+ D = mexTrainDL(X2,param);
 t=toc;
 fprintf('time of computation for Dictionary Learning: %f\n',t);
-ImD=displayPatches(D);
+% ImD=displayPatches(D);
 % parameter of the optimization procedure are chosen
-param.L=12; % not more than 20 non-zeros coefficients (default: min(size(D,1),size(D,2)))
-% param.eps=0.0;
+% param.L=1; % not more than 20 non-zeros coefficients (default: min(size(D,1),size(D,2)))
+param.eps=0.0;
 param.lambda=0.15; % not more than 20 non-zeros coefficients
-% param.numThreads=8; % number of processors/cores to use; the default choice is -1
-                    % and uses all the cores of the machine
 param.mode=2;       % penalized formulation
 
 tic
-alpha=mexLasso(X,D,param);
+alpha=mexLasso(X1,D,param);
 t=toc;
 toc
-
-fprintf('%f signals processed per second\n',size(X,2)/t);
+fprintf('%f signals processed per second\n',size(X1,2)/t);
 
 X3 = D*alpha;
-[height, width, channels] = size(I1)
-I3 = zeros(size(I1));
-for c=1:channels
-    start = (c-1) * s_ *s_
-    ende = c * s_ *s_
-    T = (col2im(X3(start+1:ende,:), [s_  s_], [height width], 'distinct')) * 255;
-    I3(:,:,c) = T;
-end
+
+I3 = ecol2im(X3, [s_ s_], size(I1));
 
 
-imwrite(uint8(I3), '~/Bilder/factorimage_data/epitomes/fig1_orig.png.recon_sparse.jpeg');
+imwrite(uint8(I3*255), '~/Bilder/factorimage_data/epitomes/fig1_orig.png.recon_sparse.jpeg');
